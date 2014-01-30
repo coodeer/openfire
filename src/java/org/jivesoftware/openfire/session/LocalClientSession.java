22dee037d62771f39d0f2769940470c4f415fd87
flow <flow@b35dd754-fafc-0310-a699-88a17e54d16e>
2014-01-30 13:27:04 +0000

    Don't block stanzas send from the service
    Second try, prevent NPE if from is null. Fixes OF-724
    
    git-svn-id: http://svn.igniterealtime.org/svn/repos/openfire/trunk@13881 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/openfire/session/LocalClientSession.java
+++ src/java/org/jivesoftware/openfire/session/LocalClientSession.java
@@ -824,6 +824,9 @@ public class LocalClientSession extends LocalSession implements ClientSession {
      */
     @Override
 	public boolean canProcess(Packet packet) {
+        JID from = packet.getFrom();
+        // All packets from our own service are implicitly not blocked
+        if (from != null && from.getDomain().equals(serverName)) return true;
 
         PrivacyList list = getActiveList();
         if (list != null) {
